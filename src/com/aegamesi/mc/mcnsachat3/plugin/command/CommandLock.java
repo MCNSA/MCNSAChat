2e636c325dc279d0fcc15aef2bb33146a1011cac
Kenton Hamaluik <hamaluik@ualberta.ca>
2013-03-10 19:00:24 -0600

    Added optional parameter to the lock command to make players move to a channel before locking them there

--- src/com/aegamesi/mc/mcnsachat3/plugin/command/CommandLock.java
+++ src/com/aegamesi/mc/mcnsachat3/plugin/command/CommandLock.java
@@ -9,7 +9,7 @@ import com.aegamesi.mc.mcnsachat3.packets.PlayerUpdatePacket;
 import com.aegamesi.mc.mcnsachat3.plugin.MCNSAChat3;
 import com.aegamesi.mc.mcnsachat3.plugin.PluginUtil;
 
-@Command.CommandInfo(alias = "clock", permission = "lock", usage = "<player>", description = "toggles a player's channel lock status")
+@Command.CommandInfo(alias = "clock", permission = "lock", usage = "<player> [channel]", description = "locks a player in a channel and optionally moves them")
 public class CommandLock implements Command {
 	public static MCNSAChat3 plugin = null;
 
@@ -20,26 +20,40 @@ public class CommandLock implements Command {
 	public Boolean handle(Player player, String sArgs) {
 		if(sArgs.length() < 1)
 			return false;
+
+		// process the args
+		String[] argParts = sArgs.split(" ");
 	
-		Player bukkitPlayer = Bukkit.getPlayer(sArgs);
+		Player bukkitPlayer = Bukkit.getPlayer(argParts[0]);
 		if(bukkitPlayer == null) {
 			PluginUtil.send(player.getName(), "&cPlayer not found.");
 			return true;
 		}
-		
-		ChatPlayer p = PlayerManager.getPlayer(bukkitPlayer.getName(), plugin.name);
-		if(p.modes.contains(ChatPlayer.Mode.LOCKED)) {
-			PluginUtil.send(p.name, "You are no longer locked from changing channels.");
-			p.modes.remove(ChatPlayer.Mode.LOCKED);
-		} else {
-			PluginUtil.send(p.name, "You have locked from changing channels.");
-			p.modes.add(ChatPlayer.Mode.LOCKED);
+
+		// get the player
+		ChatPlayer targetPlayer = PlayerManager.getPlayer(bukkitPlayer.getName(), plugin.name);
+
+		// check to see if there is the optional channel parameters
+		if(argParts.length > 1) {
+			// move them straight to the channel, do not check permissions
+			targetPlayer.changeChannels(argParts[1]);
 		}
 		
-		PluginUtil.send(player.getName(), "Locked " + PluginUtil.formatUser(p.name));
+		// now toggle their locked status
+		if(targetPlayer.modes.contains(ChatPlayer.Mode.LOCKED)) {
+			PluginUtil.send(targetPlayer.name, "You are no longer locked from changing channels.");
+			targetPlayer.modes.remove(ChatPlayer.Mode.LOCKED);
+		} else {
+			PluginUtil.send(targetPlayer.name, "You have locked from changing channels.");
+			targetPlayer.modes.add(ChatPlayer.Mode.LOCKED);
+		}
 		
+		// notify them
+		PluginUtil.send(player.getName(), "Locked " + PluginUtil.formatUser(targetPlayer.name));
+		
+		// and update them across the servers
 		if (MCNSAChat3.thread != null)
-			MCNSAChat3.thread.write(new PlayerUpdatePacket(p));
+			MCNSAChat3.thread.write(new PlayerUpdatePacket(targetPlayer));
 		
 		return true;
 	}
